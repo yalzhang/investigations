0681b23bebe552dd917c9c2ac72c0aecf4ae1052
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2025-06-27 18:03:04 +0200

    scripts: add uninstall_vm
    Add script to be able to remove vms that are running in the host.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

---
+++ scripts/uninstall_vm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -xe
+
+while getopts "n:" opt; do
+  case $opt in
+	n) vm_name=$OPTARG ;;
+	\?) echo "Invalid option"; exit 1 ;;
+  esac
+done
+
+if [ -z "${vm_name}" ]; then
+	echo "Please, specify the VM name"
+	exit 1
+fi
+
+virsh destroy ${vm_name}
+virsh undefine ${vm_name} --nvram --managed-save
