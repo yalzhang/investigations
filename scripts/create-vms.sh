590b9d5a03773a704fc14ca49e7f71b4ec8e865a
Timothée Ravier <tim@siosm.fr>
2025-08-01 15:20:24 +0200

    scripts/create-vms: Download a fixed stable FCOS image

--- scripts/create-vms.sh
+++ scripts/create-vms.sh
@@ -5,7 +5,8 @@ set -e
 KEY=$1
 TRUSTEE_SSH_PORT=2222
 VM_SSH_PORT=2223
-IMAGE=$(pwd)/fcos-cvm.tar-qemu.x86_64.qcow2
+STABLE_IMAGE="$(pwd)/fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2"
+CUSTOM_IMAGE="$(pwd)/fcos-cvm.tar-qemu.x86_64.qcow2"
 TRUSTEE_PORT=8080
 
 if [ -z "$KEY" ]; then
@@ -13,19 +14,37 @@ if [ -z "$KEY" ]; then
 	exit 1
 fi
 
-scripts/install_vm.sh  -n trustee  -b configs/trustee.bu -k "$(cat $KEY)" -f  -p ${TRUSTEE_SSH_PORT} \
-	-i ${IMAGE} -t ${TRUSTEE_PORT}
+# Download a fixed stable image that matches the one used for the container build with trustee
+wget https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/42.20250705.3.0/x86_64/fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2.xz
+unxz fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2.xz
+
+scripts/install_vm.sh \
+	-n trustee \
+	-b configs/trustee.bu \
+	-k "$(cat $KEY)" \
+	-f \
+	-p ${TRUSTEE_SSH_PORT} \
+	-i ${STABLE_IMAGE} \
+	-t ${TRUSTEE_PORT}
 
 until curl http://127.0.0.2:${TRUSTEE_PORT}; do
   echo "Waiting for KBS to be available..."
   sleep 1
 done
-until ssh core@localhost -p ${TRUSTEE_SSH_PORT} -i "${KEY%.*}" \
-  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-  'sudo /usr/local/bin/populate_kbs.sh'; do
+until ssh core@localhost \
+	-p ${TRUSTEE_SSH_PORT} \
+	-i "${KEY%.*}" \
+	-o StrictHostKeyChecking=no \
+	-o UserKnownHostsFile=/dev/null \
+	'sudo /usr/local/bin/populate_kbs.sh'; do
 	echo "Waiting for KBS to be populate"
 	sleep 1
 done
 
-scripts/install_vm.sh  -n vm  -b configs/luks.bu -k "$(cat $KEY)" -f  -p ${VM_SSH_PORT} \
-	-i ${IMAGE}
+scripts/install_vm.sh \
+	-n vm \
+	-b configs/luks.bu \
+	-k "$(cat $KEY)" \
+	-f \
+	-p ${VM_SSH_PORT} \
+	-i ${CUSTOM_IMAGE}
