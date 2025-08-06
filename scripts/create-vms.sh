dcdb25a0c162124a1c005bb41734848c37b0c665
Timothée Ravier <tim@siosm.fr>
2025-08-06 16:57:05 +0200

    scripts: Misc cleanups

--- scripts/create-vms.sh
+++ scripts/create-vms.sh
@@ -6,7 +6,7 @@ KEY=$1
 TRUSTEE_SSH_PORT=2222
 VM_SSH_PORT=2223
 STABLE_IMAGE="$(pwd)/fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2"
-CUSTOM_IMAGE="$(pwd)/fcos-cvm.tar-qemu.x86_64.qcow2"
+CUSTOM_IMAGE="$(pwd)/fcos-cvm-qemu.x86_64.qcow2"
 TRUSTEE_PORT=8080
 
 if [ -z "$KEY" ]; then
@@ -14,9 +14,11 @@ if [ -z "$KEY" ]; then
 	exit 1
 fi
 
-# Download a fixed stable image that matches the one used for the container build with trustee
-wget https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/42.20250705.3.0/x86_64/fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2.xz
-unxz fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2.xz
+if [[ ! -f "${STABLE_IMAGE}" ]]; then
+    # Download a fixed stable image that matches the one used for the container build with trustee
+    wget "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/42.20250705.3.0/x86_64/fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2.xz"
+    unxz "fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2.xz"
+fi
 
 scripts/install_vm.sh \
 	-n trustee \
