eed74f43ec732c6073de83a804eee301dd63023a
Timothée Ravier <tim@siosm.fr>
2025-09-17 13:22:02 +0200

    scripts: Split demo script into distinct steps

---
+++ scripts/create-trustee-vm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+# set -x
+
+if [[ "${#}" -ne 1 ]]; then
+	echo "Usage: $0 <path-to-ssh-public-key>"
+	exit 1
+fi
+
+KEY=$1
+STABLE_IMAGE="$(pwd)/fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2"
+
+if [[ ! -f "${STABLE_IMAGE}" ]]; then
+    # Download a fixed stable image that matches the one used for the container build with trustee
+    wget "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/42.20250705.3.0/x86_64/fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2.xz"
+    unxz "fedora-coreos-42.20250705.3.0-qemu.x86_64.qcow2.xz"
+fi
+
+scripts/install_vm.sh \
+	-n trustee \
+	-b configs/trustee.bu \
+	-k "$(cat $KEY)" \
+	-f \
+	-i "${STABLE_IMAGE}"
---
+++ scripts/populate-trustee-kbs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -euo pipefail
+# set -x
+
+if [[ "${#}" -ne 1 ]]; then
+	echo "Usage: $0 <path-to-ssh-public-key>"
+	exit 1
+fi
+
+KEY=$1
+IP="$(./scripts/get-ip.sh trustee)"
+TRUSTEE_PORT=8080
+
+# Setup reference values, policies and secrets
+until curl "http://${IP}:${TRUSTEE_PORT}"; do
+	echo "Waiting for KBS to be available..."
+	sleep 1
+done
+until ssh core@$IP \
+	-i "${KEY%.*}" \
+	-o StrictHostKeyChecking=no \
+	-o UserKnownHostsFile=/dev/null \
+	'sudo /usr/local/bin/populate_kbs.sh'; do
+	echo "Waiting for KBS to be populated..."
+	sleep 1
+done
+
+# Setup remote ignition config
+BUTANE=pin-trustee.bu
+IGNITION="${BUTANE%.bu}.ign"
+
+sed "s/<IP>/$IP/" configs/remote-ign/${BUTANE} > tmp/${BUTANE}
+
+podman run --interactive --rm --security-opt label=disable \
+	--volume "$(pwd)/tmp:/pwd" \
+	--workdir /pwd \
+	quay.io/confidential-clusters/butane:clevis-pin-trustee \
+	--pretty --strict /pwd/$BUTANE --output "/pwd/$IGNITION"
+
+scp -i "${KEY%.*}" \
+	-o StrictHostKeyChecking=no \
+	-o UserKnownHostsFile=/dev/null \
+	tmp/${IGNITION} core@$IP:
+
+ssh core@$IP \
+	-i "${KEY%.*}" \
+	-o StrictHostKeyChecking=no \
+	-o UserKnownHostsFile=/dev/null \
+	"sudo mv $IGNITION /srv/www && sudo systemctl restart nginx.service"
